dde710ff3c01ccc1760ce74b8ede2928747d1a6c
Jack Christensen <jack@jackchristensen.com>
2016-05-13 14:33:11 -0500

    Add list views command

--- cmd/doublecheck/main.go
+++ cmd/doublecheck/main.go
@@ -2,46 +2,49 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
+	"github.com/jackc/doublecheck"
 	"github.com/jackc/pgx"
+	"github.com/spf13/cobra"
 )
 
+const VERSION = "0.0.1"
+
+type Config struct {
+	ConnPoolConfig pgx.ConnPoolConfig
+	Schema         string
+}
+
+var cliOptions struct {
+	host     string
+	port     uint16
+	user     string
+	password string
+	database string
+	schema   string
+}
+
 func main() {
-	config, err := extractConfig()
-	if err != nil {
-		log.Fatalln(err)
+	cmdVersion := &cobra.Command{
+		Use:   "version",
+		Short: "Print version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("doublecheck v%s\n", VERSION)
+		},
 	}
 
-	pool, err := pgx.NewConnPool(config)
-	if err != nil {
-		log.Fatalln(err)
+	cmdList := &cobra.Command{
+		Use:   "list",
+		Short: "Print views doublecheck will use",
+		Run:   List,
 	}
+	addConfigFlagsToCommand(cmdList)
 
-	rows, err := pool.Query("select * from test_pgx() as t(a);")
-
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		fmt.Printf("doesn't get here")
-
-		var n *int32
-		err = rows.Scan(&n)
-		if err != nil {
-			fmt.Printf(err.Error())
-		} else {
-			fmt.Printf("%d\n", *n)
-		}
-	}
-
-	if rows.Err() != nil {
-		fmt.Println(rows.Err())
-	}
-
+	rootCmd := &cobra.Command{Use: "doublecheck", Short: "doublecheck - data validator"}
+	rootCmd.AddCommand(cmdVersion)
+	rootCmd.AddCommand(cmdList)
+	rootCmd.Execute()
 }
 
 func extractConfig() (config pgx.ConnPoolConfig, err error) {
@@ -67,16 +70,67 @@ func extractConfig() (config pgx.ConnPoolConfig, err error) {
 	return config, nil
 }
 
-type  struct {
-	schemaName      string
-	validationViews []string
-	pool            *pgx.ConnPool
+func addConfigFlagsToCommand(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(&cliOptions.host, "host", "", "", "database host")
+	cmd.Flags().Uint16VarP(&cliOptions.port, "port", "", 0, "database port")
+	cmd.Flags().StringVarP(&cliOptions.user, "user", "", "", "database user")
+	cmd.Flags().StringVarP(&cliOptions.password, "password", "", "", "database password")
+	cmd.Flags().StringVarP(&cliOptions.database, "database", "", "", "database name")
+	cmd.Flags().StringVarP(&cliOptions.schema, "schema", "", "doublecheck", "version table name")
 }
 
-func NewValidator(schemaName string, pool *pgx.ConnPool) (*Validator, error) {
+func List(cmd *cobra.Command, args []string) {
+	config, err := LoadConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading config:\n  %v\n", err)
+		os.Exit(1)
+	}
 
+	pool, err := pgx.NewConnPool(config.ConnPoolConfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to PostgreSQL:\n  %v\n", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
+	dc, err := doublecheck.New(&doublecheck.Config{ConnPool: pool, SchemaName: config.Schema})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to initialize doublecheck:\n  %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, view := range dc.Views() {
+		fmt.Println(view)
+	}
 }
 
-func (v *Validator) SchemaName() string {
-	return schemaName
+func LoadConfig() (*Config, error) {
+	config := &Config{}
+	if connConfig, err := pgx.ParseEnvLibpq(); err == nil {
+		config.ConnPoolConfig.ConnConfig = connConfig
+	} else {
+		return nil, err
+	}
+
+	appendConfigFromCLIArgs(config)
+
+	return config, nil
+}
+
+func appendConfigFromCLIArgs(config *Config) {
+	if cliOptions.host != "" {
+		config.ConnPoolConfig.Host = cliOptions.host
+	}
+	if cliOptions.port != 0 {
+		config.ConnPoolConfig.Port = cliOptions.port
+	}
+	if cliOptions.database != "" {
+		config.ConnPoolConfig.Database = cliOptions.database
+	}
+	if cliOptions.user != "" {
+		config.ConnPoolConfig.User = cliOptions.user
+	}
+	if cliOptions.password != "" {
+		config.ConnPoolConfig.Password = cliOptions.password
+	}
 }
